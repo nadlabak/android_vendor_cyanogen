8a28a79df24fa32fc5c513214660700831f80d18
nadlabak <pavel@doshaska.net>
2011-10-08 10:22:19 +0200

    don't include CMWalpapers in case of odexed build
    Change-Id: I4102344e768b9153562b766d85461cd5b0e45337

--- products/common_full.mk
+++ products/common_full.mk
@@ -1,9 +1,11 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/cyanogen/products/common.mk)
 
+ifndef WITH_DEXPREOPT
 # CyanogenMod specific product packages
 PRODUCT_PACKAGES += \
     CMWallpapers
+endif
 
 # Bring in all audio files
 include frameworks/base/data/sounds/AllAudio.mk

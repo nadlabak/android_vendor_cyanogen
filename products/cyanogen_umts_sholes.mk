a06eaecb8f807f1f05d4a8734c83f77d0ed4760f
nadlabak <pavel@doshaska.net>
2011-10-17 23:29:07 +0200

    umts_sholes: fix BUILD_DISPLAY_ID
    Change-Id: Ib38c5f5d4a39fd1e2c55e603a4176098b6a48858

--- products/cyanogen_umts_sholes.mk
+++ products/cyanogen_umts_sholes.mk
@@ -22,16 +22,14 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := umts_sholes
 PRODUCT_MODEL := Milestone
 PRODUCT_MANUFACTURER := Motorola
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=SHOLS_U2_05.26.3 PRODUCT_NAME=umts_sholes TARGET_DEVICE=umts_sholes BUILD_FINGERPRINT=MOTO_RTEU/umts_sholes/umts_sholes/sholes:2.2.1/SHOLS_U2_05.26.3/296482885:user/release-keys PRODUCT_BRAND=MOTO_RTEU PRIVATE_BUILD_DESC="umts_sholes-user 2.2.1 SHOLS_U2_05.26.3 296482885 release-keys" BUILD_NUMBER=296482885 BUILD_UTC_DATE=1295422848 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := Milestone
 PRODUCT_VERSION_DEVICE_SPECIFIC := .1
 -include vendor/cyanogen/products/common_versions.mk
 
-PRODUCT_BUILD_PROP_OVERRIDES := BUILD_ID=SHOLS_U2_05.26.3 PRODUCT_NAME=umts_sholes TARGET_DEVICE=umts_sholes BUILD_FINGERPRINT=MOTO_RTEU/umts_sholes/umts_sholes/sholes:2.2.1/SHOLS_U2_05.26.3/296482885:user/release-keys PRODUCT_BRAND=MOTO_RTEU PRIVATE_BUILD_DESC="umts_sholes-user 2.2.1 SHOLS_U2_05.26.3 296482885 release-keys" BUILD_NUMBER=296482885 BUILD_UTC_DATE=1295422848 TARGET_BUILD_TYPE=user BUILD_VERSION_TAGS=release-keys USER=android-build
-
 # PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-umts_sholes.map
-
 
 PRODUCT_COPY_FILES +=  \
     vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
